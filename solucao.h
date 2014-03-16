f7192e6215cba9e0083526556a5bada59d4b4719
j0bs <gmcrivelli@gmail.com>
2014-03-16 17:07:47 -0300

    Versão final do módulo de solução.

---
+++ solucao.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<pthread.h>
+
+#define N_THR 100
+
+typedef struct {
+	char matriz[9][9];
+	int x, y, id;
+} Input;
+
+void addToStack(int i);
+int getFromStack();
+int testaPossibilidade(char sudoku[9][9], int x, int y, int v);
+void* backtrack_thread(int id);
+void* f_thread(void* p);
+void imprime(char mat[9][9]);
+int rodaSolucao();
